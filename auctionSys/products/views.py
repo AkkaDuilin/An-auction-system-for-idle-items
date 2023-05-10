4d3b56056ab444cbdce1ac85d5e01e7a35aaeed6
AkkaDuilin <2075861530@qq.com>
2023-05-10 17:32:53 +0800

    add readme and Todo

--- auctionSys/products/views.py
+++ auctionSys/products/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ProductInfo,ProductType
 
 # Create your views here.
 def index(request):
-
-
     return render(request, 'product/index.html')
     #return HttpResponse("this is product index page")
+    
+    
+def plist(request, type_id, page, sort):
+     return render(request, 'product/list.html')
+ 
+def detail(request, id):
+    res = render(request, 'product/detail.html')
+    view_products = request.COOKIES.get('view_products', '')
+    res.set_cookie('view_products', view_products)
+    return res
